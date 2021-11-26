ed5edb4ffd6ecf5f79e9cf366ec1ad4cbe557eb7
rambo99 <tiaocarreiro99@gmail.com>
2021-11-26 14:10:13 -0300

    Testando a primeira classe de domonio

--- src/main/java/com/example/cursomc/CursomcApplication.java
+++ src/main/java/com/example/cursomc/CursomcApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication {
 
-	// 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
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
