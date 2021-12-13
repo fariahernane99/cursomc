ea2ecf2832d37461e4b935f34ba05f73972c03d7
fariahernane99 <fariahernane@gmail.com>
2021-12-13 13:05:42 -0300

    Endpoint /pedidos/id disponibilizado

--- src/main/java/com/example/cursomc/domain/ItemPedidoPK.java
+++ src/main/java/com/example/cursomc/domain/ItemPedidoPK.java
@@ -7,36 +7,44 @@ import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 // classe de associação
 @Embeddable
 public class ItemPedidoPK implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "pedido_id")
 	private Pedido pedido;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "produto_id")
 	private Produto produto;
-	
+
+	@JsonIgnore
 	public Pedido getPedido() {
 		return pedido;
 	}
+
 	public void setPedido(Pedido pedido) {
 		this.pedido = pedido;
 	}
+
+	@JsonIgnore
 	public Produto getProduto() {
 		return produto;
 	}
+
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(pedido, produto);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
